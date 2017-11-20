93c9bb40b940b0b2fc4af26575d827bd8dd8995b
marat-gainullin <mg@altsoft.biz>
2017-11-20 16:13:08 +0300

    Ensurance commit

--- karma.conf.js
+++ karma.conf.js
@@ -40,12 +40,12 @@ module.exports = function (config) {
                         inlineImages: true
                     }
                 ]
-                //,['browserify-babel-istanbul']
+                ,['browserify-babel-istanbul']
             ]
         },
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
-        singleRun: false,
+        singleRun: true,
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
